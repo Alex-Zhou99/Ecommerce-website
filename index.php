96c70ce684b0ac3f8082750cb5e0d21affa5277a
shi-edward <shi_edward@icloud.com>
2016-08-10 17:37:44 -0500

    43

--- index.php
+++ index.php
@@ -54,7 +54,16 @@ include("functions/functions.php");
                 <?php cart(); ?>
                 <div id="shopping_cart">
                     <span style="float:right; font-size:18px; padding:5px; line-height:40px;">
-                        Welcome Guest! <b style="color:yellow">Shopping Cart-</b>Total Items: <?php total_items(); ?>  Total Price: <?php total_price(); ?><a href="cart.php" style="color:yellow"> Go to Cart</a>
+                        <?php 
+                            if(isset($_SESSION['customer_email'])){
+                                echo "<b>Welcome:</b>" . $_SESSION['customer_email'] . "<b style='color:yellow;'>Your</b>" ;
+                            }else {
+                                echo "<b>Welcome Guest:</b>";
+                            }
+                        ?>
+                        
+                        
+                        <b style="color:yellow">Shopping Cart-</b>Total Items: <?php total_items(); ?>  Total Price: <?php total_price(); ?><a href="cart.php" style="color:yellow"> Go to Cart</a>
                         
                         <?php 
                             if(!isset($_SESSION['customer_email'])){
