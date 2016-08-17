16c04b0abbc99ec1ef433763b12099854c798cda
shi-edward <shi_edward@icloud.com>
2016-08-16 23:56:13 -0500

    48

--- customers/edit_account.php
+++ customers/edit_account.php
@@ -1,4 +1,19 @@
-
+<?php
+    include("includes/db.php"); 			
+    $user = $_SESSION['customer_email'];			
+    $get_customer = "select * from customers where customer_email='$user'";			
+    $run_customer = mysqli_query($con, $get_customer); 			
+    $row_customer = mysqli_fetch_array($run_customer); 		
+    $c_id = $row_customer['customer_id'];
+    $name = $row_customer['customer_name'];
+    $email = $row_customer['customer_email'];
+    $pass = $row_customer['customer_pass'];
+    $country = $row_customer['customer_country'];
+    $city = $row_customer['customer_city'];
+    $contact = $row_customer['customer_contact'];
+    $address= $row_customer['customer_address'];
+    $image = $row_customer['customer_image'];
+?>
                     <form action="customer_register.php" method="post" enctype="multipart/form-data">
 					    <table align="center" width="750">
                             <tr align="center">
@@ -6,28 +21,28 @@
                             </tr>
                             <tr>
                                 <td align="right">Customer Name:</td>
-                                <td><input type="text" name="c_name" required/></td>
+                                <td><input type="text" name="c_name" value="<?php echo $name;?>" required/></td>
                             </tr>
 
                             <tr>
                                 <td align="right">Customer Email:</td>
-                                <td><input type="text" name="c_email" required/></td>
+                                <td><input type="text" name="c_email" value="<?php echo $email;?>" required/></td>
                             </tr>
 
                             <tr>
                                 <td align="right">Customer Password:</td>
-                                <td><input type="password" name="c_pass" required/></td>
+                                <td><input type="password" name="c_pass" value="<?php echo $pass;?>" required/></td>
                             </tr>
 
                             <tr>
                                 <td align="right">Customer Image:</td>
-                                <td><input type="file" name="c_image" required/></td>
+                                <td><input type="file" name="c_image"/><img src="customer_images/<?php echo $image; ?>" width="50" height="50"/></td>
                             </tr>
                             <tr>
                                 <td align="right">Customer Country:</td>
                                 <td>
                                 <select name="c_country">
-                                    <option>Select a Country</option>
+                                    <option><?php echo $country; ?></option>
                                     <option>Afghanistan</option>
                                     <option>India</option>
                                     <option>Japan</option>
@@ -43,17 +58,17 @@
 
                             <tr>
                                 <td align="right">Customer City:</td>
-                                <td><input type="text" name="c_city" required/></td>
+                                <td><input type="text" name="c_city" value="<?php echo $city;?>"/></td>
                             </tr>
 
                             <tr>
                                 <td align="right">Customer Contact:</td>
-                                <td><input type="text" name="c_contact" required/></td>
+                                <td><input type="text" name="c_contact" value="<?php echo $contact;?>"/></td>
                             </tr>
 
                             <tr>
                                 <td align="right">Customer Address</td>
-                                <td><input type="text" name="c_address" required/></td>
+                                <td><input type="text" name="c_address" value="<?php echo $address;?>"/></td>
                             </tr>
                             <tr align="center">
                                 <td colspan="6"><input type="submit" name="register" value="Update Your Account" /></td>
