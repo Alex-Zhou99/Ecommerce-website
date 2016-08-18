f81e3712e178bebdd7e292f4f7fae4b7f49a56b0
shi-edward <shi_edward@icloud.com>
2016-08-18 04:07:52 -0500

    paypal

--- payment.php
+++ payment.php
@@ -1,5 +1,13 @@
 <div>
-    <h2 align="center">Pay now with Paypal:</h2>
-    <p style="text-align:center"><img src="paypal.png" width="200" height="100"/></p>
+    <h2 align="center" style="padding:2px;">Pay now with Paypal:</h2>
 
+    <form action="https://www.sandbox.paypal.com/cgi-bin/webscr" method="post">
+  <input type="hidden" name="cmd" value="_s-xclick">
+  <input type="hidden" name="hosted_button_id" value="6RNT8A4HBBJRE">
+  <input type="image"
+    src="paypal_img.png" width="250" height="150"
+    name="submit" alt="PayPal - The safer, easier way to pay online!">
+  <img alt="" src="https://www.sandbox.paypal.com/en_US/i/scr/pixel.gif"
+    width="1" height="1">
+</form>
 </div>
